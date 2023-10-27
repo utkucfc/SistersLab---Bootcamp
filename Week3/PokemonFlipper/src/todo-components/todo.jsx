758cf5830a308cf8cf1a0bcecac71aaeedad70f0
Utku Çifci <utku.cifci@gmail.com>
2023-10-27 18:01:17 +0300

    css updated

--- Week3/PokemonFlipper/src/todo-components/todo.jsx
+++ Week3/PokemonFlipper/src/todo-components/todo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./todo.css";
+import "./font.css"
 
 const Todos = (() => {
     const [task, setTask] = useState('');
@@ -21,19 +22,22 @@ const Todos = (() => {
 
     return (
         <div className="main">
-            <h1>Todo List</h1>
-            <div className="add">
-                <input type="text" value={task} onChange={(event) => setTask(event.target.value)} />
-                <button onClick={AddTodos} disabled={task === '' ? true : false}>Add</button>
+            <h1>To-Do List</h1>
+            <div className="content">
+                <div className="add">
+                    <input type="text" value={task} onChange={(event) => setTask(event.target.value)} />
+                    <button onClick={AddTodos} disabled={task === '' ? true : false}>Add</button>
+                </div>
+                <ul>
+                    {
+                        todos.map((todo, index) => (
+                            <li key={index}><div><div className="todo">{todo}</div>
+                                <div className="buttons"><button onClick={() => DeleteTodos(index)}>Delete</button>
+                                    <button onClick={() => EditTodos(index)} disabled={task === '' ? true : false}>Edit</button></div>
+                            </div>
+                            </li>))}
+                </ul>
             </div>
-            <ul>
-                {
-                    todos.map((todo, index) => (
-                        <li key={index}><div className="todo">{todo}</div>
-                            <div className="buttons"><button onClick={() => DeleteTodos(index)}>Delete</button>
-                                <button onClick={() => EditTodos(index)} disabled={task === '' ? true : false}>Edit</button></div>
-                        </li>))}
-            </ul>
         </div>
     )
 })
